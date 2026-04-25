9dd73f46a123354f32757ccb9ee35cfc3cf6da76
DuyTran <tanhduy160620@gmail.com>
2026-04-26 03:40:04 +1000

    [Scenari-Aid]-[new-features/#03-ui-settings]: + Structure routes for the website

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,41 @@
+import { Routes, Route } from "react-router-dom";
+
+// layouts
+import HomeLayout from "@/layouts/HomeLayout";
+import AccountLayout from "@/layouts/AccountLayout";
+
+// pages
+import HomePage from "@/pages/home/HomePage";
+import AboutPage from "@/pages/about/AboutPage";
+import TutorialPage from "@/pages/tutorial/TutorialPage";
+import ContactPage from "@/pages/contact/ContactPage";
+import TermsConditionsPage from "@/pages/terms&conditions/TermsConditionsPage";
+import LoginPage from "@/pages/account/LoginPage";
+import RegisterPage from "@/pages/account/RegisterPage";
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route element={<HomeLayout />}>
+                <Route path="/" element={<HomePage />}></Route>
+            </Route>
+
+            <Route path="/about">
+                <Route index element={<AboutPage />} />
+                {/* This routes will be replaced later */}
+                <Route path="sub-page-1" />
+                <Route path="sub-page-2" />
+            </Route>
+
+            <Route path="/tutorial" element={<TutorialPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/terms" element={<TermsConditionsPage />} />
+            <Route path="/account" element={<AccountLayout />}>
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<RegisterPage />} />
+            </Route>
+        </Routes>
+    );
+};
+
+export default AppRoutes;
