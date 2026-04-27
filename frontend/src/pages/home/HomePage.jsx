dddf3ed9da8611057f734b16c7605a7e52f18b49
DuyTran <tanhduy160620@gmail.com>
2026-04-27 17:28:44 +1000

    [Scenari-Aid]-[new-features/#11-build-user-feedback-component]: + Implement Our Users Feedback component

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,7 +1,7 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Box, Button, Container, Grid, GridItem, Heading } from "@chakra-ui/react";
 import Banner from "./Banner";
 import VideoCarousel from "@/components/common/carousels/VideoCarousel";
-
+import UsersFeedbackGrid from "@/components/common/grids/UsersFeedbackGrid";
 
 function HomePage() {
     return (
@@ -12,6 +12,13 @@ function HomePage() {
                     NEW SCENARIOS
                 </Heading>
                 <VideoCarousel />
+                <Heading color="brand.500" fontFamily="Sora" fontWeight={"bold"} fontSize={48}>
+                    OUR USERS FEEDBACK
+                </Heading>
+                <Box spaceY={5}>
+                    <UsersFeedbackGrid />
+                    <Button size={"sm"} color={"white"} bg={"brand.500"} fontFamily={"Sora"}>View All</Button>
+                </Box>
             </Container>
         </div>
     );
