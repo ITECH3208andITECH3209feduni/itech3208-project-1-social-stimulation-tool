a5ba1e72b8589bb76e073b777c68792dee07ba15
DuyTran <tanhduy160620@gmail.com>
2026-04-27 19:21:46 +1000

    [Scenari-Aid]-[new-features/#12-build-footer-component]: + Modified SocialBar with Link component

--- frontend/src/components/common/socialbar/SocialBar.jsx
+++ frontend/src/components/common/socialbar/SocialBar.jsx
@@ -1,24 +1,32 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, HStack, Link } from "@chakra-ui/react";
 import { FaYoutube, FaFacebook, FaInstagram, FaLinkedin, FaTiktok } from "react-icons/fa";
+import { fedYoutube, fedFacebook, fedInstagram, fedLinkin, fedTikTok } from "@/assets/fedUrl";
+
+const socials = [
+    { icon: FaYoutube, url: fedYoutube },
+    { icon: FaFacebook, url: fedFacebook },
+    { icon: FaInstagram, url: fedInstagram },
+    { icon: FaLinkedin, url: fedLinkin },
+    { icon: FaTiktok, url: fedTikTok },
+];
 
 function SocialBar() {
     return (
         <HStack fontSize={25} gap={2}>
-            <Box color="navy.500">
-                <FaYoutube />
-            </Box>
-            <Box color="navy.500">
-                <FaFacebook />
-            </Box>
-            <Box color="navy.500">
-                <FaInstagram />
-            </Box>
-            <Box color="navy.500">
-                <FaLinkedin />
-            </Box>
-            <Box color="navy.500">
-                <FaTiktok />
-            </Box>
+            {socials.map(({ icon: Icon, url }, index) => (
+                <Link key={index} href={url} isExternal>
+                    <Box
+                        color="navy.500"
+                        _hover={{
+                            color: "brand.500",
+                            transform: "scale(1.2)",
+                        }}
+                        transition="0.2s"
+                    >
+                        <Icon />
+                    </Box>
+                </Link>
+            ))}
         </HStack>
     );
 }
