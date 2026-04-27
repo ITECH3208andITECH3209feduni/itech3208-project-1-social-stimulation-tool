f5f3af0aded0a534e3b88dd12a963ce741daf064
Andrew <>
2026-04-27 18:46:06 +1000

    [Scenari-Aid]-[new-features/#09-account-page-build]: + Create authentication field configurations + Create authentication form component

---
+++ frontend/src/components/auth/authConfig.js
@@ -0,0 +1,13 @@
+// All the fields for the register and login form
+export const loginFields = [
+    { label: "Username", placeholder: "Enter Email or Username", type: "text" },
+    { label: "Password", placeholder: "Enter password", type: "password" },
+];
+
+export const registerFields = [
+    { label: "Username", placeholder: "Enter Username", type: "text" },
+    { label: "Email", placeholder: "Enter Email", type: "email" },
+    { label: "Phone", placeholder: "Enter Phone Number", type: "text" },
+    { label: "Password", placeholder: "Enter password", type: "password" },
+    { label: "Confirm Password", placeholder: "Confirm your password", type: "password" },
+];
