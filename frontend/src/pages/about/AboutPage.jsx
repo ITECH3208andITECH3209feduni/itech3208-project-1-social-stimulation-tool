8714dde7822e16ec4bc4143dd07837147937c031
DuyTran <tanhduy160620@gmail.com>
2026-04-28 13:31:54 +1000

    [Scenari-Aid]-[new-features/#15-build-about-page]: + Implement About Page with created components

--- frontend/src/pages/about/AboutPage.jsx
+++ frontend/src/pages/about/AboutPage.jsx
@@ -1,5 +1,16 @@
+import { Container } from "@chakra-ui/react";
+import AboutUs from "./AboutUs";
+import OurMission from "./OurMission";
+import OurTeam from "./OurTeam";
+
 function AboutPage() {
-    return <div>AboutPage</div>;
+    return (
+        <Container spaceY={"50px"}>
+            <AboutUs />
+            <OurMission />
+            <OurTeam />
+        </Container>
+    );
 }
 
 export default AboutPage;
