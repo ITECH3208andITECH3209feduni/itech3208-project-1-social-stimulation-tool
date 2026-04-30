4a1ae2d7c12a21e8491fdcddaee152ac64b2dcd0
DuyTran <tanhduy160620@gmail.com>
2026-04-30 22:40:37 +1000

    [Scenari-Aid]-[admin-features/#04-build-tutorials-page]: + Implement Tutorial Page with created components

--- admin/src/pages/tutorials/TutorialPage.jsx
+++ admin/src/pages/tutorials/TutorialPage.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { VStack, Pagination, IconButton, ButtonGroup } from "@chakra-ui/react";
+import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
+import VideoGrid, { videoList } from "@/components/common/grids/VideoGrid";
 
 function TutorialPage() {
-    return <div>TutorialPage</div>;
+    return (
+        <VStack gap={8}>
+            <VideoGrid />
+            {/* Pagination */}
+            <Pagination.Root count={videoList.length * 5} pageSize={5} page={1}>
+                <ButtonGroup variant="ghost" size="sm" wrap="wrap">
+                    <Pagination.PrevTrigger asChild>
+                        <IconButton>
+                            <LuChevronLeft />
+                        </IconButton>
+                    </Pagination.PrevTrigger>
+
+                    <Pagination.Items
+                        render={(page) => (
+                            <IconButton variant={{ base: "ghost", _selected: "outline" }}>
+                                {page.value}
+                            </IconButton>
+                        )}
+                    />
+
+                    <Pagination.NextTrigger asChild>
+                        <IconButton>
+                            <LuChevronRight />
+                        </IconButton>
+                    </Pagination.NextTrigger>
+                </ButtonGroup>
+            </Pagination.Root>
+        </VStack>
+    );
 }
 
 export default TutorialPage;
