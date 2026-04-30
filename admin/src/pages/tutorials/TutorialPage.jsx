2ae110ee35fbde31846ca08db82c7af72c35e57e
DuyTran <tanhduy160620@gmail.com>
2026-05-01 01:45:50 +1000

    [Scenari-Aid]-[admin-features/#08-build-contact-page]: + Add tutorial heading for tutorial page

--- admin/src/pages/tutorials/TutorialPage.jsx
+++ admin/src/pages/tutorials/TutorialPage.jsx
@@ -1,10 +1,13 @@
-import { VStack, Pagination, IconButton, ButtonGroup } from "@chakra-ui/react";
+import { VStack, Pagination, IconButton, ButtonGroup, Heading } from "@chakra-ui/react";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 import VideoGrid, { videoList } from "@/components/common/grids/VideoGrid";
 
 function TutorialPage() {
     return (
-        <VStack gap={8}>
+        <VStack gap={6}>
+            <Heading alignSelf={"start"} color={"brand.400"} fontSize={48}>
+                Tutorial Management
+            </Heading>
             <VideoGrid />
             {/* Pagination */}
             <Pagination.Root count={videoList.length * 5} pageSize={5} page={1}>
