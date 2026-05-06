37dcb699238dbd44f0bcd2d092420f7f3c67528d
DuyTran <tanhduy160620@gmail.com>
2026-05-07 00:42:55 +1000

    [Scenari-Aid]-[backend-features/#06-implement-add-levels]: + Modified level route and level schema for validate schema

--- backend/src/features/levels/level.validation.js
+++ backend/src/features/levels/level.validation.js
@@ -1,8 +1,12 @@
 import Joi from "joi";
 import LevelMessages from "./level.message.js";
 
-const levelSchema = Joi.object({
+const insertLevel = Joi.object({
     name: Joi.string().required().min(3).max(50).messages(LevelMessages.validation),
 });
 
-export default levelSchema;
+const LevelSchema = {
+    insert: insertLevel
+}
+
+export default LevelSchema;
