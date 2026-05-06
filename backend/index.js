674a8056925ee15fed6e56c651c291d6f9830f2f
DuyTran <tanhduy160620@gmail.com>
2026-05-06 21:57:01 +1000

    [Scenari-Aid]-[backend-features/#03-implement-add-categories]: + Modified import + Modified sendError function in updated response util + Register Category Router

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 // MARK: - Standard Libraries
 import express from "express";
-import env from "#config/env.config.js";
 import { StatusCodes } from "http-status-codes";
 
 // MARK: - Packages Customization
+import { envConfig, apiConfig, mongoConfig } from "#config/index.js";
 import { loggerUtil, resUtil } from "#utils/index.js";
-import mongoConfig from "#config/mongodb.config.js";
+import { CategoryRouter } from "#routes/index.js";
 
 const app = express();
 
@@ -14,26 +14,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // MARK: - App API Routes
-// TODO: - Register Routes Here!!!
+app.use(apiConfig.category, CategoryRouter);
 
 // MARK: - Handle 404 Not Found
-app.use(env.apiPrefix, (error, req, res, next) => {
+app.use(envConfig.apiPrefix, (error, req, res, next) => {
     loggerUtil.error(`Handle general error: ${error.stack}`);
-    resUtil.sendError(res, "Page Not Found", StatusCodes.NOT_FOUND);
+    resUtil.sendError({ res, message: "Page Not Found", statusCode: StatusCodes.NOT_FOUND });
 });
 
 // MARK: - Handle General Error
 app.use((error, req, res, next) => {
     loggerUtil.error(`Handle general error: ${error.stack}`);
-    resUtil.sendError(res, "Internal Server Error", StatusCodes.INTERNAL_SERVER_ERROR);
+    resUtil.sendError({ res, message: "Internal Server Error", statusCode: StatusCodes.NOT_FOUND });
 });
 
 // MARK: - Main Application
 const runApp = async () => {
     try {
         await mongoConfig.connect();
-        app.listen(env.port, () => {
-            loggerUtil.debug(`Server is run on port ${env.port}`);
+        app.listen(envConfig.port, () => {
+            loggerUtil.debug(`Server is run on port ${envConfig.port}`);
         });
     } catch (error) {
         loggerUtil.error(`Application run failed: ${error}`);
