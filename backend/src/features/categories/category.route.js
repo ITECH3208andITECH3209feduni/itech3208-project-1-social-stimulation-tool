ac28f1e8b29d121b20b849098feacb75974d3090
DuyTran <tanhduy160620@gmail.com>
2026-05-07 00:42:34 +1000

    [Scenari-Aid]-[backend-features/#06-implement-add-levels]: + Modified category route and category for validate schema

--- backend/src/features/categories/category.route.js
+++ backend/src/features/categories/category.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import CategoryController from "./category.controller.js";
 import bodyMw from "#middlewares/body.middleware.js";
-import categorySchema from "./category.validation.js";
+import CategorySchema from "./category.validation.js";
 
 const router = express.Router();
 
 router.get("/", CategoryController.getCategories);
-router.post("/insert-category", bodyMw.validate(categorySchema), CategoryController.insertCategory);
+router.post("/insert-category", bodyMw.validate(CategorySchema.insert), CategoryController.insertCategory);
 router.post("/bulk-insert-categories", CategoryController.bulkInsertCategories);
 router.delete("/delete-category/:id", CategoryController.deleteCategory)
 
