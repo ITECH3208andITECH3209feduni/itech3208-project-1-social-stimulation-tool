7bf94c1154e0eb7719dbd916e61c5a64b2680694
DuyTran <tanhduy160620@gmail.com>
2026-05-07 00:57:52 +1000

    [Scenari-Aid]-[backend-features/#07-implement-get-levels]: + Add api endpoint for get levels

--- backend/src/features/levels/level.route.js
+++ backend/src/features/levels/level.route.js
@@ -5,6 +5,7 @@ import LevelSchema from "./level.validation.js";
 
 const router = express.Router();
 
+router.get("/", LevelController.getLevels);
 router.post("/insert-level", bodyMw.validate(LevelSchema.insert), LevelController.insertLevel);
 router.post("/bulk-insert-levels", LevelController.bulkInsertLevels);
 
