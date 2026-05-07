036cd7b7da7e96dd99dfc5e02ab62c73aacdf5b1
DuyTran <tanhduy160620@gmail.com>
2026-05-07 21:43:32 +1000

    [Scenari-Aid]-[backend-features/#11-implement-upload-video]: + Mount VideoRouter on main app + Standardize 404 and global error handlers

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import { StatusCodes } from "http-status-codes";
 // MARK: - Packages Customization
 import { envConfig, apiConfig, mongoConfig } from "#config/index.js";
 import { loggerUtil, resUtil } from "#utils/index.js";
-import { CategoryRouter, LevelRouter, AuthRouter, UserRouter } from "#routes/index.js";
+import { CategoryRouter, LevelRouter, AuthRouter, UserRouter, VideoRouter } from "#routes/index.js";
 
 const app = express();
 
@@ -18,17 +18,25 @@ app.use(apiConfig.category, CategoryRouter);
 app.use(apiConfig.level, LevelRouter);
 app.use(apiConfig.auth, AuthRouter);
 app.use(apiConfig.user, UserRouter);
+app.use(apiConfig.video, VideoRouter);
 
 // MARK: - Handle 404 Not Found
-app.use(envConfig.apiPrefix, (error, req, res, next) => {
-    loggerUtil.error(`Handle general error: ${error.stack}`);
-    resUtil.sendError({ res, message: "Page Not Found", statusCode: StatusCodes.NOT_FOUND });
+app.use((req, res, next) => {
+    resUtil.sendError({
+        res,
+        message: "Page Not Found",
+        statusCode: StatusCodes.NOT_FOUND,
+    });
 });
 
 // MARK: - Handle General Error
 app.use((error, req, res, next) => {
-    loggerUtil.error(`Handle general error: ${error.stack}`);
-    resUtil.sendError({ res, message: "Internal Server Error", statusCode: StatusCodes.NOT_FOUND });
+    loggerUtil.error(`[HandleGeneralError]: ${error.stack}`);
+    resUtil.sendError({
+        res,
+        message: error.message || "Internal Server Error",
+        statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    });
 });
 
 // MARK: - Main Application
