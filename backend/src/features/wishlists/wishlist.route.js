76c3ede4b7f471d249143a2e0086634f90e325c9
DuyTran <tanhduy160620@gmail.com>
2026-05-07 23:00:14 +1000

    [Scenari-Aid]-[backend-features/#14-implement-wishlist]: + [NEW] Define wishlist routes protected by authentication

---
+++ backend/src/features/wishlists/wishlist.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { authMw } from "#middlewares/index.js";
+import WishlistController from "./wishlist.controller.js";
+
+const router = express.Router();
+
+// PATCH /api/v1/wishlists/toggle/:videoId — Toggle video in wishlist
+router.patch(
+    "/toggle/:videoId",
+    authMw.authorizeRole([authMw.UserRole.user, authMw.UserRole.admin]),
+    WishlistController.toggleWishlist
+);
+
+// GET /api/v1/wishlists — Get user wishlist
+router.get(
+    "/",
+    authMw.authorizeRole([authMw.UserRole.user, authMw.UserRole.admin]),
+    WishlistController.getWishlist
+);
+
+export default router;
