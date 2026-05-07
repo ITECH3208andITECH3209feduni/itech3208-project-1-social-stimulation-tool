9cd9a3c894ba5a1eb32a9f30c27fa680f4a97cf5
DuyTran <tanhduy160620@gmail.com>
2026-05-07 14:05:03 +1000

    [Scenari-Aid]-[backend-features/#09-implement-login-user]: + Implement get user infor in user controller

--- backend/src/features/users/user.controller.js
+++ backend/src/features/users/user.controller.js
@@ -1,5 +1,28 @@
+import { StatusCodes } from "http-status-codes";
+import { loggerUtil, resUtil } from "#utils/index.js";
 import UserService from "./user.service.js";
+import UserMessages from "./user.message.js";
 
-const UserController = {};
+const UserController = {
+    getUserInfor: async (req, res) => {
+        try {
+            const userId = req.user.id;
+            const userInfo = await UserService.getUserInfor(userId)
+            resUtil.sendSuccess({
+                res,
+                message: UserMessages.success.GET_USER_INFOR_SUCCESSFULLY,
+                data: userInfo
+            })
+        } catch (error) {
+            loggerUtil.error(`[UserController.getUserInfor]: ${error}`);
+            return resUtil.sendError({
+                res,
+                message: error.message,
+                statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+                errorCode: error.errorCode,
+            });
+        }
+    },
+};
 
 export default UserController;
