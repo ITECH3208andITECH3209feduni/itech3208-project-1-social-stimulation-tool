5e2b0d5b13df155dae49eecc292debb272956601
DuyTran <tanhduy160620@gmail.com>
2026-05-07 14:05:42 +1000

    [Scenari-Aid]-[backend-features/#09-implement-login-user]: + Implement login user in auth service

--- backend/src/features/auth/auth.service.js
+++ backend/src/features/auth/auth.service.js
@@ -1,5 +1,5 @@
 import UserService from "#features/users/user.service.js";
-import { bcryptUtil } from "#utils/index.js";
+import { bcryptUtil, jwtUtil } from "#utils/index.js";
 import AuthMessages from "./auth.message.js";
 
 const AuthService = {
@@ -13,6 +13,24 @@ const AuthService = {
         const newUser = await UserService.insertUser({ ...userData, password: hashedPassword });
 
         return newUser;
+    },
+
+    loginUser: async (input) => {
+        const validUser = await UserService.checkCredential(input);
+        
+        const payload = {
+            id: validUser._id,
+            avatar: validUser.avatar,
+            firstName: validUser.firstName,
+            lastName: validUser.lastName,
+            role: validUser.role,
+        };
+
+        const accessToken = jwtUtil.genAccessToken(payload);
+
+        return {
+            accessToken,
+        };
     },
 };
 
