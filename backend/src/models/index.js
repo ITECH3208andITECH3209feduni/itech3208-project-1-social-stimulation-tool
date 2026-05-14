7066f6108eac70974bc0a282dd49aba11751c492
Andrew <>
2026-05-14 22:54:19 +1000

    [scenario-aid]-[backend-features/#20-add-organization-mongodb-schema]: + import organisation model to index.js from model folder

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -2,6 +2,7 @@ import CategoryModel from "./category.model.js";
 import ContactModel from "./contact.model.js";
 import FeedbackModel from "./feedback.model.js";
 import LevelModel from "./level.model.js";
+import OrganizationModel from "./organization.model.js";
 import UserModel from "./user.model.js";
 import VideoModel from "./video.model.js";
 import WishlistModel from "./wishlist.model.js";
@@ -11,6 +12,7 @@ export {
     ContactModel,
     FeedbackModel,
     LevelModel,
+    OrganizationModel,
     UserModel,
     VideoModel,
     WishlistModel,
