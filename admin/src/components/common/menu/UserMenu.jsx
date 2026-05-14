ef66a0e3344ff286403abb3ec7eb152b53500fbe
Senan-Sn69 <senansasnuka1@gmail.com>
2026-05-15 01:45:52 +1000

    [Scenari-Aid]-[admin-features/#10-build-avatar-logout]:
    +Add a popup menu for logout on user account avatar

---
+++ admin/src/components/common/menu/UserMenu.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import { Menu, Avatar, Text, Portal, Button, Box, Image } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+function UserMenu({ displayName }) {
+    const navigate = useNavigate();
+
+    return (
+        <Box w="40px" h="40px" borderRadius="full" bg="brand.500" color="white">
+            <Menu.Root>
+                <Menu.Trigger asChild>
+                    <Image
+                        src="/images/user_account.png"
+                        alt="User"
+                        w="100%"
+                        h="100%"
+                        objectFit="cover"
+                    />
+                </Menu.Trigger>
+                <Portal>
+                    <Menu.Positioner>
+                        <Menu.Content bg={"white"}>
+                            <Menu.Item value="export" >Logout</Menu.Item>
+                        </Menu.Content>
+                    </Menu.Positioner>
+                </Portal>
+            </Menu.Root>
+        </Box>
+    );
+}
+
+UserMenu.propTypes = {
+    displayName: PropTypes.string.isRequired,
+};
+
+export default UserMenu;
