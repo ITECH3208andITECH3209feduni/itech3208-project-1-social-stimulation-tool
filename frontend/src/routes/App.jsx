aaff792d62a825bc897df2a71b4689af6acd0859
DuyTran <tanhduy160620@gmail.com>
2026-06-01 02:42:41 +1000

    [Scenari-Aid]-[new-features/#23-implement-check-expired-token]: + Add component Toasted at the root level

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
     return (
         <Router>
             <AppRoutes />
+            <Toaster />
         </Router>
     );
 }
