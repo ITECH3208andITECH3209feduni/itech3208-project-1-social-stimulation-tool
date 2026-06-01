a4953562b11a28c29d54ea6a45dcfe0d6aee7174
DuyTran <tanhduy160620@gmail.com>
2026-06-01 16:49:49 +1000

    [Scenari-Aid]-[new-features/#24-implement-check-for-use-tutorial-page]: + Implement checking required auth for accessing tutorial page

--- frontend/src/pages/tutorial/TutorialPage.jsx
+++ frontend/src/pages/tutorial/TutorialPage.jsx
@@ -1,24 +1,34 @@
-import { Container, GridItem, Heading, VStack, Grid } from "@chakra-ui/react";
+import { Container, GridItem, Heading, VStack, Grid, Center } from "@chakra-ui/react";
 import TutorialGrid from "@/components/common/grids/TutorialGrid";
 import LevelFilter from "@/components/common/filters/LevelFilter";
 import CategoryFilter from "@/components/common/filters/CategoryFilter";
 import TutorialPagination from "@/components/common/paginations/TutorialPagination";
+import useAuthStore from "@/hooks/stores/useAuthStore";
+import RequiredAuth from "@/components/auth/RequiredAuth";
 
 function TutorialPage() {
+    const { accessToken } = useAuthStore();
+
     return (
         <Container mt={"100px"} spaceY={"50px"}>
-            <Heading color="brand.500" fontFamily="Sora" fontWeight={"bold"} fontSize={48}>
-                Welcome to our tutorial
-            </Heading>
-            <VStack align={"start"} gap={8}>
-                {/* Category Filter */}
-                <CategoryFilter />
+            {accessToken ? (
+                <>
+                    <Heading color="brand.500" fontFamily="Sora" fontWeight={"bold"} fontSize={48}>
+                        Welcome to our tutorial
+                    </Heading>
+                    <VStack align={"start"} gap={8}>
+                        {/* Category Filter */}
+                        <CategoryFilter />
 
-                {/* Level Filter */}
-                <LevelFilter />
-            </VStack>
-            <TutorialGrid />
-            <TutorialPagination />
+                        {/* Level Filter */}
+                        <LevelFilter />
+                    </VStack>
+                    <TutorialGrid />
+                    <TutorialPagination />
+                </>
+            ) : (
+                <RequiredAuth />
+            )}
         </Container>
     );
 }
