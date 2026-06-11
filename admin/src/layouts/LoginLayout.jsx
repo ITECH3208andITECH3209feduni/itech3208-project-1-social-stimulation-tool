bab312506addcea897f38485d11691aad7bb4508
DuyTran <tanhduy160620@gmail.com>
2026-06-11 23:37:30 +1000

    [Scenari-Aid]-[admin-features/#12-implement-login-admin]: + Update login layout with Flex component

--- admin/src/layouts/LoginLayout.jsx
+++ admin/src/layouts/LoginLayout.jsx
@@ -1,12 +1,14 @@
-import { Container } from "@chakra-ui/react";
+import { Toaster } from "@/components/ui/toaster";
+import { Flex } from "@chakra-ui/react";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 function LoginLayout() {
     return (
-        <Container>
+        <Flex justify={"center"} align={"center"}>
             <Outlet />
-        </Container>
+            <Toaster />
+        </Flex>
     );
 }
 
