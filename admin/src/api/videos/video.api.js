1d3f7925696b1d3ff494254cddfe83d40f0d40bd
DuyTran <tanhduy160620@gmail.com>
2026-06-14 02:31:26 +1000

    [Scenari-Aid]-[admin-features/#14-implement-upload-video]: + Add API endpoint for get categories, create video, upload video + Implement create video and upload video for fetching API

--- admin/src/api/videos/video.api.js
+++ admin/src/api/videos/video.api.js
@@ -1,6 +1,34 @@
 import axiosInstance from "../axios";
 import API_ROUTES from "../api.config";
 
-const videoApi = {};
+const videoApi = {
+    createVideo: async (payload) => {
+        const res = await axiosInstance.post(API_ROUTES.ME.CREATE_VIDEO, payload, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+
+        return {
+            success: res.success,
+            message: res.message,
+            data: res.data,
+        };
+    },
+
+    uploadVideo: async (payload) => {
+        const res = await axiosInstance.post(API_ROUTES.ME.UPLOAD_VIDEO, payload, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+
+        return {
+            success: res.success,
+            message: res.message,
+            data: res.data,
+        };
+    },
+};
 
 export default videoApi;
