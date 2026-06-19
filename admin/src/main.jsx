9b4808f127c265f827200aac2392583f9aef79e1
DuyTran <tanhduy160620@gmail.com>
2026-06-20 02:22:52 +1000

    [Scenari-Aid]-[admin-features/#17-implement-video-pagination]: + Implement query client with tanstack-query + Re-name for url path

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,14 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "@/routes/App";
 import system from "./theme";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ChakraProvider value={system}>
-            <App />
-        </ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider value={system}>
+                <App />
+            </ChakraProvider>
+        </QueryClientProvider>
     </StrictMode>,
 );
