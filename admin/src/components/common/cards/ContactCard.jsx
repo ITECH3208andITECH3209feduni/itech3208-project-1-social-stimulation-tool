2fc2eeb7722955330d6ec623dc7eb1dd7f9acbae
DuyTran <tanhduy160620@gmail.com>
2026-07-01 22:59:35 +1000

    [Scenari-Aid]-[admin-features/#18-implement-contact-pagination]: + Modify attributes for ContactCard component

--- admin/src/components/common/cards/ContactCard.jsx
+++ admin/src/components/common/cards/ContactCard.jsx
@@ -1,14 +1,16 @@
 import StatusBadge from "@/components/badges/StatusBadge";
 import { Card, Text, VStack, HStack, Icon, Badge, Spacer, Button } from "@chakra-ui/react";
 import { FaEnvelope, FaPhone, FaMapMarkerAlt, FaCircle } from "react-icons/fa";
+import UserAvatar from "@/components/common/avatar/UserAvatar";
 
 function ContactCard({ contact }) {
     return (
         <Card.Root bg="dark.800" color="whiteAlpha.900" rounded="md" h={"350px"}>
             {/* Header */}
             <Card.Header>
-                <HStack>
-                    <Card.Title fontSize="xl">{contact.name}</Card.Title>
+                <HStack gap={3}>
+                    <UserAvatar src={contact.user.avatar?.url} name={contact.user.username} size="md" />
+                    <Card.Title fontSize="xl">{contact.user.username}</Card.Title>
                     <Spacer />
                     <StatusBadge status={contact.status} />
                 </HStack>
@@ -20,28 +22,28 @@ function ContactCard({ contact }) {
                     {/* Email */}
                     <HStack fontSize="sm" color="gray.300">
                         <Icon as={FaEnvelope} />
-                        <Text>{contact.email}</Text>
+                        <Text>{contact.user.email}</Text>
                     </HStack>
 
                     {/* Phone (optional) */}
-                    {contact.phone && (
+                    {contact.user.phone && (
                         <HStack fontSize="sm" color="gray.300">
                             <Icon as={FaPhone} />
-                            <Text>{contact.phone}</Text>
+                            <Text>{contact.user.phone}</Text>
                         </HStack>
                     )}
 
                     {/* Address (optional) */}
-                    {contact.address && (
+                    {contact.user.address && (
                         <HStack fontSize="sm" color="gray.300">
                             <Icon as={FaMapMarkerAlt} />
-                            <Text>{contact.address}</Text>
+                            <Text>{contact.user.address}</Text>
                         </HStack>
                     )}
 
                     {/* Description */}
                     <Text fontSize="sm" color="gray.400">
-                        {contact.description}
+                        {contact.message}
                     </Text>
                 </VStack>
             </Card.Body>
