2981d6cb3cb7f411935b95837c5245cf7e120a18
DuyTran <tanhduy160620@gmail.com>
2026-07-06 23:41:55 +1000

    [Scenari-Aid]-[admin-features/#19-implement-user-pagination]: + Modified roles in get videos of public route + Add managing users of admin route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,15 @@
 import { CategoryPublicRouter, CategoryAdminRouter } from "#features/categories/category.route.js";
 import { LevelPublicRouter, LevelAdminRouter } from "#features/levels/level.route.js";
 import AuthRouter from "#features/auth/auth.route.js";
-import UserRouter from "#features/users/user.route.js";
-import { VideoPublicRouter, VideoUserRouter, VideoAdminRouter } from "#features/videos/video.route.js";
+import { UserRouter, UserAdminRouter } from "#features/users/user.route.js";
+import {
+    VideoPublicRouter,
+    VideoUserRouter,
+    VideoAdminRouter,
+} from "#features/videos/video.route.js";
 import FeedbackRouter from "#features/feedbacks/feedback.route.js";
 import WishlistRouter from "#features/wishlists/wishlist.route.js";
-import ContactRouter from "#features/contacts/contact.route.js";
+import { ContactPublicRouter, ContactAdminRouter } from "#features/contacts/contact.route.js";
 
 export {
     CategoryPublicRouter,
@@ -14,10 +18,12 @@ export {
     LevelAdminRouter,
     AuthRouter,
     UserRouter,
+    UserAdminRouter,
     VideoPublicRouter,
     VideoUserRouter,
     VideoAdminRouter,
     FeedbackRouter,
     WishlistRouter,
-    ContactRouter
+    ContactPublicRouter,
+    ContactAdminRouter,
 };
