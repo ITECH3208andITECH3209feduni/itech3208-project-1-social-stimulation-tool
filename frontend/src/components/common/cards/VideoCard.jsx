0896c24610a0d46d28369eb2e3144c1aa18bbd16
AzureAD\ArnishKhandelwal <Arnish2000@gmail.com>
2026-08-10 19:04:00 +1000

    [Secnari-Aid]-[new-features/#31-update-images-in-home-page]: + Modified variable for injecting image in component

--- frontend/src/components/common/cards/VideoCard.jsx
+++ frontend/src/components/common/cards/VideoCard.jsx
@@ -1,10 +1,16 @@
 import { Box, Image } from "@chakra-ui/react";
-import { imgScenario } from "@/assets";
 
-function VideoCard() {
+function VideoCard({imgSrc}) {
     return (
-        <Box w={"100%"} h={"100%"} bg={"red"}>
-            <Image src={imgScenario} w={"100%"} h={"100%"} objectFit={"cover"} />
+        <Box w={"100%"} h={"100%"} >
+            <Image 
+                src={imgSrc} 
+                w={"100%"}
+                borderRadius={10}
+                objectFit={"cover"}
+                objectPosition={"center"}
+                display={"block"}
+            /> 
         </Box>
     );
 }
