ca187a1ce62b270608db783e55deddf778ee97c7
Senan-Sn69 <senansasnuka1@gmail.com>
2026-08-12 01:35:37 +1000

    [Scenari-Aid]-[new-features/#32-rebuild-tutorial-page-with-video-placeholder]: +Remove pagination from tutorial page

--- frontend/src/pages/tutorial/TutorialPage.jsx
+++ frontend/src/pages/tutorial/TutorialPage.jsx
@@ -2,7 +2,6 @@ import { Container, GridItem, Heading, VStack, Grid, Center } from "@chakra-ui/r
 import TutorialGrid from "@/components/common/grids/TutorialGrid";
 import LevelFilter from "@/components/common/filters/LevelFilter";
 import CategoryFilter from "@/components/common/filters/CategoryFilter";
-import TutorialPagination from "@/components/common/paginations/TutorialPagination";
 import useAuthStore from "@/hooks/stores/useAuthStore";
 import RequiredAuth from "@/components/auth/RequiredAuth";
 
@@ -16,15 +15,9 @@ function TutorialPage() {
                     <Heading color="brand.500" fontFamily="Sora" fontWeight={"bold"} fontSize={48}>
                         Welcome to our tutorial
                     </Heading>
-                    <VStack align={"start"} gap={8}>
-                        {/* Category Filter */}
-                        <CategoryFilter />
-
-                        {/* Level Filter */}
-                        <LevelFilter />
-                    </VStack>
+                    
                     <TutorialGrid />
-                    <TutorialPagination />
+                    
                 </>
             ) : (
                 <RequiredAuth />
