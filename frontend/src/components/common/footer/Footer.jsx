1d1a4263b57e42f18216d4b842d751d64941c934
DuyTran <tanhduy160620@gmail.com>
2026-09-06 23:04:25 +1000

    [Scenari-Aid]-[origin/new-features/#34-optimize-home-page]: + Remove the old footer + Build the new footer

--- frontend/src/components/common/footer/Footer.jsx
+++ frontend/src/components/common/footer/Footer.jsx
@@ -1,51 +1,53 @@
-import { HStack, Text, VStack, Image, Flex } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import { federationLogo } from "@/assets";
+import { Link, VStack, Image, Flex, Text, HStack } from "@chakra-ui/react";
+import { footerImg } from "@/assets";
 import React from "react";
-import SocialBar from "../socialbar/SocialBar";
+import HighlightText from "@/pages/terms&conditions/HighlightText";
+import { useNavigate } from "react-router-dom";
+
+const footerUrl =
+    "https://federation.edu.au/schools/school-of-engineering-information-technology-and-physical-sciences/research/computational-science-and-mathematics/ciao/research-groups/technologies-for-empowering-people-for-participation-in-society-tepps";
 
 function Footer() {
+    const navigate = useNavigate();
+
+    const goToTermsandConditions = () => navigate("/terms");
+
     return (
         <Flex
             as={"nav"}
             h={"80px"}
-            px={"100px"}
-            py={"100px"}
+            py={"80px"}
             align={"center"}
-            justify={"space-between"}
+            justify={"center"}
             gap={"6"}
             bg={"white"}
             color={"black"}
         >
-            <Link to="/">
-                <Image
-                    src={federationLogo}
-                    alt="Federation University"
-                    h={"60px"}
-                    cursor={"pointer"}
-                />
-            </Link>
-            <VStack align={"start"}>
-                <Text fontFamily={"Sora"} fontSize={16} fontWeight={"bold"}>
-                    DOMESTIC
-                </Text>
-                <Text color={"navy.500"} fontFamily={"Sora"} fontSize={30} fontWeight={"bold"}>
-                    1800 FED UNI (1800 333 864)
-                </Text>
-            </VStack>
-            <VStack align={"start"}>
-                <Text fontFamily={"Sora"} fontSize={16} fontWeight={"bold"}>
-                    INTERNATIONAL
-                </Text>
-                <Text color={"navy.500"} fontFamily={"Sora"} fontSize={30} fontWeight={"bold"}>
-                    Contact Us
-                </Text>
-            </VStack>
-            <VStack align={"start"}>
-                <Text fontFamily={"Sora"} fontSize={16} fontWeight={"bold"}>
-                    FIND US
-                </Text>
-                <SocialBar />
+            <VStack align={"center"}>
+                <HStack>
+                    <Text fontSize={"12px"} fontWeight={"semibold"}>
+                        Copyright © 2026. Grant Meredith |
+                    </Text>
+                    <HighlightText
+                        color="skyblue.500"
+                        fontSize="12px"
+                        text={
+                            "Developed by Stimulation Tool Upgrade Group | Read our Terms & Conditions"
+                        }
+                        highlights={[
+                            { text: "Terms & Conditions", action: goToTermsandConditions },
+                        ]}
+                    />
+                </HStack>
+
+                <Link href={footerUrl} target="_blank" rel="noopener noreferrer">
+                    <Image
+                        src={footerImg}
+                        alt="Federation University"
+                        h={"60px"}
+                        cursor={"pointer"}
+                    />
+                </Link>
             </VStack>
         </Flex>
     );
