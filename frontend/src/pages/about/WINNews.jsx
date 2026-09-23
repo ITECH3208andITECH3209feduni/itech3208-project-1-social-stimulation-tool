cd097d41da56fe95436d1bd33b4d2d64a9ba2263
DuyTran <tanhduy160620@gmail.com>
2026-09-23 22:44:48 +1000

    [Scenari-Aid]-[new-features/#41-redesign-about-page]: + Redesign WINNews section

--- frontend/src/pages/about/WINNews.jsx
+++ frontend/src/pages/about/WINNews.jsx
@@ -1,38 +1,109 @@
-import { aboutUs2, winNew } from "@/assets";
-import { Box, Flex, Heading, Image, Text, VStack, ListItem, Link, AspectRatio, Icon, List } from "@chakra-ui/react";
-import { FaCrown, FaPlayCircle } from "react-icons/fa";
+import { winNew } from "@/assets";
+import { AspectRatio, Box, Grid, Heading, Link, Text } from "@chakra-ui/react";
 
 function WINNews() {
-  return (
-    <Box mt={16} fontFamily={"Sora"} color={"navy.500"} fontSize={18}>
-        <VStack spacing={6} align="start">
-            <Box w="100%" bg="gray.50" p={8} rounded="xl" borderLeft="4px solid" borderColor="brand.500">
-                <Flex align="center" gap={4} mb={4}>
-                    <Icon as={FaPlayCircle} boxSize={8} color="red.500" />
-                    <Heading size="md" color="brand.500">WIN News Feature</Heading>
-                </Flex>
-                <Text>
-                    This Video is a news report of the Scenari-Aid DVD by Australia's National WIN News.
+    return (
+        <Box>
+            <Box mb={8}>
+                <Text
+                    fontSize="sm"
+                    fontWeight="600"
+                    letterSpacing="wide"
+                    textTransform="uppercase"
+                    color="brand.500"
+                    mb={3}
+                >
+                    Media Coverage
                 </Text>
-                <AspectRatio maxW="600px" w="100%" ratio={16 / 9} mt={4} rounded="md" overflow="hidden" bg="black">
+
+                <Heading
+                    fontFamily="Sora"
+                    fontSize={{ base: "3xl", md: "4xl" }}
+                    color="gray.800"
+                    mb={5}
+                >
+                    WIN News Feature
+                </Heading>
+
+                <Text
+                    maxW="800px"
+                    color="gray.600"
+                    fontSize={{ base: "md", md: "lg" }}
+                    lineHeight="1.8"
+                >
+                    A WIN News report featuring the Scenari-Aid DVD and the development of the
+                    project.
+                </Text>
+            </Box>
+
+            <Grid
+                templateColumns={{
+                    base: "1fr",
+                    lg: "1fr 1fr",
+                }}
+                gap={{ base: 8, lg: 12 }}
+                alignItems="center"
+            >
+                <AspectRatio
+                    ratio={16 / 9}
+                    w="100%"
+                    maxW="900px"
+                    borderRadius="xl"
+                    overflow="hidden"
+                    bg="black"
+                >
                     <Box as="video" controls src={winNew} w="100%" h="100%" objectFit="contain" />
                 </AspectRatio>
+
+                <Box textAlign={"left"} mt={8}>
+                    <Text color="gray.600" fontSize={{ base: "md", md: "lg" }} lineHeight="1.8">
+                        Scenari-Aid is now a free website containing over 100 staged streaming video
+                        scenarios for people to work through in their own time, pace and purpose of
+                        need. We wholeheartedly thank the Telematics Trust for financially backing
+                        this phase of the project.
+                    </Text>
+
+                    <Text
+                        mt={5}
+                        color="gray.600"
+                        fontSize={{ base: "md", md: "lg" }}
+                        lineHeight="1.8"
+                    >
+                        The 100 initial scenarios were shot within Ballarat, Victoria, Australia.
+                        Everybody you see within the scenarios are volunteers and nobody is an
+                        actor.
+                    </Text>
+                </Box>
+            </Grid>
+
+            <Box
+                mt={8}
+                p={6}
+                borderLeft="3px solid"
+                borderLeftColor="brand.500"
+                borderRight="1px solid"
+                borderRightColor="gray.500"
+                borderTop="1px solid"
+                borderTopColor="gray.500"
+                borderBottom="1px solid"
+                borderBottomColor="gray.500"
+                bg="gray.50"
+                borderRadius="md"
+            >
+                <Text color="gray.800" lineHeight="1.7">
+                    You will need to register to access the scenarios.{" "}
+                    <Link color="brand.500" fontWeight="600" href="/account/register">
+                        Register here
+                    </Link>
+                    . If you have any questions or suggestions, please{" "}
+                    <Link color="brand.500" fontWeight="600" href="/contact">
+                        contact us
+                    </Link>
+                    .
+                </Text>
             </Box>
-            <Text>
-                Scenari-Aid is now a free website containing over 100 staged streaming video scenarios for people to work through in their own time, pace and purpose of need. We wholeheartedly thank the Telematics Trust for financially backing this phase of the project. The 100 initial scenarios were shot within Ballarat, Victoria, Australia. Everybody you see within the scenarios are volunteers and nobody is an actor. Most of the people you see are authentic within their scenario.
-            </Text>
-
-            <Text>
-                These scenarios offer the user a perfect platform to work through in regards to their purpose and need. Each scenario is comprised of a number of steps in which the user can pause at any time and choose to respond in a time and manner that best fits their situation, style & needs.
-            </Text>
-
-            <Text bg="blue.50" p={6} rounded="lg" w="100%" border="1px solid" borderColor="blue.100">
-                You will need to register to be able to access the scenarios. Registration is available through{" "}
-                <Link color="brand.500" fontWeight="bold" href="/account/register">this link</Link>. If you have any questions or suggestions about Scenari-Aid please do not hesitate to <Link color="brand.500" fontWeight="bold" href="/contact">contact us</Link>.
-            </Text>
-        </VStack>
-    </Box>
-  )
+        </Box>
+    );
 }
 
-export default WINNews
+export default WINNews;
