62b18447dfeffe0006adb230f0c3e078f081f391
DuyTran <tanhduy160620@gmail.com>
2026-09-23 22:43:29 +1000

    [Scenari-Aid]-[new-features/#41-redesign-about-page]: + Redesign AboutUs section

--- frontend/src/pages/about/AboutUs.jsx
+++ frontend/src/pages/about/AboutUs.jsx
@@ -1,47 +1,73 @@
-import { aboutUs2, winNew } from "@/assets";
-import { Box, Flex, Heading, Image, Text, VStack, ListItem, Link, AspectRatio, Icon, List } from "@chakra-ui/react";
-import { FaCrown, FaPlayCircle } from "react-icons/fa";
+import { aboutUs2 } from "@/assets";
+import { Box, Heading, Image, Link, Text, VStack } from "@chakra-ui/react";
+
+const GrantMeredithInfoUrl =
+    "https://www.federation.edu.au/research/find-an-expert/grant-meredith/";
 
 function AboutUs() {
     return (
-        <Box mt={50} maxW="7xl" mx="auto">
-            {/* MARK: About Section */}
-            <Flex direction={{ base: "column", lg: "row" }} gap={10} alignItems="flex-start">
-                <Box flex={1} fontFamily={"Sora"} textAlign={"left"}>
-                    <Heading color={"brand.500"} fontSize={{ base: 36, md: 44 }} mb={6}>
-                        About Scenari-Aid
-                    </Heading>
-
-                    <VStack spacing={5} align="start" color={"navy.500"} fontSize={18}>
-                        <Text>
-                            Scenari-Aid is an application designed by <Link color="brand.500" fontWeight="bold" href="/contact">Grant Meredith</Link> who is a lecturer within the School of Science, Information Technology & Engineering (SITE) at Federation University Australia. Grant leads the Technologies for Empowering People for Participation in Society (TEPPS) programme which conceive, designs and evaluate “assertive technologies” for people at need.
-                        </Text>
-
-                        <Text>
-                            Originally designed as a free DVD application for people who stutter and with financial aid from the Helen Macpherson Smith Trust, over the past 2 years close to 1,000 users have since benefited from the Scenari-Aid for a range of diverse reasons. These reasons have included fluency and speech therapy for people, stroke recovery, literacy skills for people from non-English speaking backgrounds, addressing social anxiety issues and the empowering the long term unemployed.
-                        </Text>
-                    </VStack>
-                </Box>
-
-                <Box
-                    flex={1}
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    w="100%"
-                    rounded="xl"
-                    overflow={"hidden"}
-                    boxShadow="2xl"
+        <Box>
+            <VStack align="start" gap={6}>
+                <Text
+                    fontSize="sm"
+                    fontWeight="600"
+                    letterSpacing="wide"
+                    textTransform="uppercase"
+                    color="brand.500"
                 >
-                    <Image
-                        src={aboutUs2}
-                        w={"100%"}
-                        objectFit="cover"
-                        alt="About Scenari-Aid"
-                    />
-                </Box>
-            </Flex>
-           
+                    About the Project
+                </Text>
+
+                <Heading
+                    fontFamily="Sora"
+                    fontSize={{ base: "3xl", md: "5xl" }}
+                    lineHeight="1.1"
+                    color="gray.800"
+                >
+                    About Scenari-Aid
+                </Heading>
+
+                <Text
+                    textAlign={"left"}
+                    fontSize={{ base: "md", md: "lg" }}
+                    lineHeight="1.8"
+                    color="gray.600"
+                >
+                    Scenari-Aid is an application designed by{" "}
+                    <Link
+                        color="brand.500"
+                        fontWeight="600"
+                        href={GrantMeredithInfoUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Grant Meredith
+                    </Link>{" "}
+                    who is a lecturer within the School of Science, Information Technology &
+                    Engineering (SITE) at Federation University Australia. Grant leads the
+                    Technologies for Empowering People for Participation in Society (TEPPS)
+                    programme which conceive, designs and evaluate “assertive technologies” for
+                    people at need.
+                </Text>
+
+                <Text
+                    textAlign={"left"}
+                    fontSize={{ base: "md", md: "lg" }}
+                    lineHeight="1.8"
+                    color="gray.600"
+                >
+                    Originally designed as a free DVD application for people who stutter and with
+                    financial aid from the Helen Macpherson Smith Trust, over the past 2 years close
+                    to 1,000 users have since benefited from Scenari-Aid for a range of diverse
+                    reasons. These reasons have included fluency and speech therapy, stroke
+                    recovery, literacy skills, social anxiety and empowering the long-term
+                    unemployed.
+                </Text>
+            </VStack>
+
+            {/* <Box mt={10} w="100%" overflow="hidden" borderRadius="xl">
+                <Image src={aboutUs2} alt="Scenari-Aid" w="100%" maxH="500px" objectFit="cover" />
+            </Box> */}
         </Box>
     );
 }
