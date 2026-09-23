9c7cab9794293a0f356fbca8a584bf29c765a71e
DuyTran <tanhduy160620@gmail.com>
2026-09-23 22:44:05 +1000

    [Scenari-Aid]-[new-features/#41-redesign-about-page]: + Redesign Crowdfunding section

--- frontend/src/pages/about/Crowdfunding.jsx
+++ frontend/src/pages/about/Crowdfunding.jsx
@@ -1,32 +1,19 @@
-import { Flex, Heading, Text, Icon } from "@chakra-ui/react";
-import { FaCrown } from "react-icons/fa";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 function Crowdfunding() {
-  return (
-     <Flex
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        textAlign="center"
-        rounded="xl"
-        mb={12}
-        py={8}
-        px={6}
-        bg="green.50"
-        color="green.800"
-        border="1px solid"
-        borderColor="green.200"
-        boxShadow="md"
-    >
-        <Icon as={FaCrown} boxSize="40px" color="yellow.500" />
-        <Heading mt={4} mb={2} fontSize="2xl" fontFamily="Sora" color={"brand.500"}>
-            Crowdfunding Success!
-        </Heading>
-        <Text maxWidth="2xl" fontSize="lg">
-            Thank you to our amazing community! With your incredible support, we've successfully reached our crowdfunding goals. Your contributions make it possible for us to keep Scenari-Aid a free and growing resource for everyone.
-        </Text>
-    </Flex>
-  )
+    return (
+        <Box py={10} px={{ base: 6, md: 10 }} bg="gray.800" color="white" borderRadius="xl">
+            <Heading fontFamily="Sora" fontSize={{ base: "2xl", md: "3xl" }} mb={4}>
+                Crowdfunding Success
+            </Heading>
+
+            <Text fontSize={{ base: "md", md: "lg" }} lineHeight="1.8" color="whiteAlpha.800">
+                Thank you to our amazing community. With your support, we successfully reached our
+                crowdfunding goals. Your contributions make it possible for us to keep Scenari-Aid a
+                free and growing resource for everyone.
+            </Text>
+        </Box>
+    );
 }
 
-export default Crowdfunding
+export default Crowdfunding;
